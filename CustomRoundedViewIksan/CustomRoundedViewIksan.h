175818a306da46724af0b3019b649f37b8184a62
iksanul15 <iksanul15@gmail.com>
2020-10-20 16:31:00 +0800

    Initial Commit

---
+++ CustomRoundedViewIksan/CustomRoundedViewIksan.h
@@ -0,0 +1,18 @@
+//
+//  CustomRoundedViewIksan.h
+//  CustomRoundedViewIksan
+//
+//  Created by Muhammad Iksanul on 20/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedViewIksan.
+FOUNDATION_EXPORT double CustomRoundedViewIksanVersionNumber;
+
+//! Project version string for CustomRoundedViewIksan.
+FOUNDATION_EXPORT const unsigned char CustomRoundedViewIksanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedViewIksan/PublicHeader.h>
+
+
